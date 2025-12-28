f94b262624fe0719ee5c7670e92eb0408b546cb2
AadityaRaj77 <aaditya200602@gmail.com>
2025-12-29 03:25:48 +0530

    db creation

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {createServer} from 'http'
+import {initSocket} from './socket.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const httpServer = createServer(app);
+initSocket(httpServer);
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
