ccfa5f7368a9221ee632415ab35617f7e8ce2c28
AadityaRaj77 <aaditya200602@gmail.com>
2026-01-01 03:11:02 +0530

    jwt auth

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.routes.js";
 import {createServer} from 'http'
 import {initSocket} from './socket.js';
 
@@ -10,14 +11,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//routes
+app.use("/auth", authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const httpServer = createServer(app);
+/*const httpServer = createServer(app);
 initSocket(httpServer);
-
+*/
 const port = 4000;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
+
